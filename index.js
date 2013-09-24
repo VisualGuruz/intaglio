86f26b25bbef45be9863ab43c0e4667eaa533884
Codey Whitt <codey.whitt@pearson.com>
2013-09-24 00:06:05 -0600

    Refactored most things to use classes and now have basic finding working

--- index.js
+++ index.js
@@ -1,40 +1,21 @@
 // This is just some test stuff for right meow
-var MySQL = require('./lib/repositories/mysql'),
-	ORM = require('./lib/orm');
+var ORM = require('./lib/orm');
 
-var repo = new MySQL({
+var mysqlRepository = new ORM.repositories.mysql({
 	host: "192.168.33.10",
 	user: "test",
 	password: "",
 	database: "test_orm"
 });
 
-var m = new ORM(repo);
-
-m.factory('foo');
-
-console.info
-
-repo.getSchema().then(function (result) {
-	console.info(result);
-	repo.find(result.bar).then(function (bar) {
-		repo.create(result.bar, {thingy: "Inserted row!"}).then(function (newRow) {
-			repo.find(result.bar).then(function (newBar) {
-				repo.save(result.bar, {thingy: "Updated row!"}, {field: 'id', value: newBar.length}).then(function (up) {
-					console.info(up);
-					repo.find(result.bar).then(function (newerBar) {
-						repo.delete(result.bar, {field: 'id', value: newerBar.length}).then(function (deleteRes) {
-							console.info(deleteRes);
-							repo.find(result.bar).then(function () {
-								process.exit(0);
-							}, err);
-						}, err);
-					}, err);
-				}, err);
-			}, err);
-		}, err);
+ORM.create(mysqlRepository).then(function (orm) {
+	orm.factory('bar').find().then(function (data) {
+		console.info(data);
 	}, err);
-}, err);
+	orm.factory('bar').find().then(function (data) {
+		console.info(data);
+	}, err);
+}).then(null, err);
 
 function err(error) {
 	console.error('Failed:', error.message, error.stack);
