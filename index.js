1c4b679f9c2d6186479594fc95bde021bd2b89c9
Codey Whitt <codey.whitt@pearson.com>
2013-10-07 08:38:16 -0600

    Added RSVP error eventing

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var RSVP = require('rsvp');
+
+RSVP.configure('onerror', function (error) {
+	console.error(error.message);
+});
+
 module.exports = {
 	repositories: require('./lib/repositories'),
 	ORM: require('./lib/orm'),
