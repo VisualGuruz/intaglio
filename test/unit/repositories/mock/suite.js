eea8d87e3b3385e160c97bcf62bcaf1949f9a39d
Codey Whitt <codey.whitt@pearson.com>
2013-12-17 19:18:33 -0700

    Updated the test files.

--- test/unit/repositories/mock/suite.js
+++ test/unit/repositories/mock/suite.js
@@ -1,8 +1,10 @@
 var MockRepository = require('./../../../../lib/repositories/mock'),
-	TestSuite = require('./../suite');
+	TestSuite = require('./../suite')
+	data = require('./../../../mock-data/data.json'),
+	schema = require('./../../../mock-data/schema.json');
 
 describe('Integration Test Suite', function () {
-	var repo = new 	MockRepository();
+	var repo = new MockRepository(schema, data);
 
 	// Pass the repository to the test suite
 	return TestSuite(repo);
